8e63bde766607fea3d3056bea27e2ba5e14fb9f9
sharman13 <58657244+sharman13@users.noreply.github.com>
2020-10-01 14:31:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Products = require('./models/Products');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb+srv://sharman:sharman010506@cluster0.tajua.mongodb.net/<node trial>?retryWrites=true&w=majority', {
     useMongoClient: true
 });
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
